1cf5494ee2ba252beb71cf496c0d0b2c5449351a
DangenBruh <smatjangir@gmail.com>
2021-06-28 19:11:21 +0600

    finally fixed the annoying dot in rtx gf

--- assets/preload/data/no-way/modchart.lua
+++ assets/preload/data/no-way/modchart.lua
@@ -6,8 +6,9 @@ function update (elapsed)
     if difficulty == 2 then
         local currentBeat = (songPos / 1000)*(bpm/60)
         for i=0,7 do
-            setActorY(defaultStrum0Y + 10 * math.cos((currentBeat + i*0.25) * math.pi), i)
-        end
+			setActorX(_G['defaultStrum'..i..'X'] + 32 * math.sin((currentBeat + i*0)), i)
+			setActorY(_G['defaultStrum'..i..'Y'],i)
+		end
     end
 end
 
